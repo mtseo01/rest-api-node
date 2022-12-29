2af280f0c91e78ec20c1ee7517daa3d2144d2ef7
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-29 16:45:52 +0900

    Handling Errors

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const productRouter = require('./api/routes/products');
 const ordersRouter = require('./api/routes/orders');
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productRouter);
 app.use('/orders', ordersRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
