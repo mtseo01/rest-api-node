12807186eb368e30a92d96a93c08f8dece03f68f
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-29 18:39:54 +0900

    Parsing body & Handling CORS

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const app = express();
 const productRouter = require('./api/routes/products');
 const ordersRouter = require('./api/routes/orders');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes which should handle requests
 app.use('/products', productRouter);
 app.use('/orders', ordersRouter);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
