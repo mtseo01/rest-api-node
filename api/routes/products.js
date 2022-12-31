6ed555774f70ef946df118edc0266cd5faf91215
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-31 18:19:59 +0900

    Add Mongoose Validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,10 +5,24 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id')
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + doc._id,
+            },
+          };
+        }),
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -26,22 +40,37 @@ router.post('/', (req, res, next) => {
     .save()
     .then((result) => {
       console.log(result);
+      res.status(201).json({
+        message: 'Created product successfully',
+        createProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products/' + result._id,
+          },
+        },
+      });
     })
     .catch((err) => console.log(err));
-
-  res.status(201).json({
-    message: 'Handling POST requests to /products',
-    createProduct: product,
-  });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select('name price _id')
     .exec()
     .then((doc) => {
       console.log('From database', doc);
-      res.status(200).json(doc);
+      res.status(200).json({
+        product: doc,
+        request: {
+          type: 'GET',
+          description: 'GET_ALL_PRODUCTS',
+          url: 'http://localhost:3000/products',
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -52,7 +81,17 @@ router.get('/:productId', (req, res, next) => {
 router.patch('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findByIdAndUpdate(id, { $set: req.body }, { new: true })
-    .then((result) => res.status(200).json(result))
+    .then((result) =>
+      res.status(200).json({
+        message: 'Product updated',
+        name: result.name,
+        price: result.price,
+        requset: {
+          type: 'GET',
+          url: 'http://localhost:3000/products/' + id,
+        },
+      })
+    )
     .catch((err) => res.status(500).json({ error: err }));
 });
 
@@ -64,7 +103,16 @@ router.delete('/:productId', (req, res, next) => {
     .then((result) => {
       console.log();
       if (result.deletedCount > 0) {
-        res.status(200).json({ message: 'Deleted Successfully' });
+        res.status(200).json({
+          message: 'Product deleted',
+          name: result.name,
+          price: result.price,
+          request: {
+            type: 'POST',
+            url: 'http://localhost:3000/products',
+            body: { name: 'String', price: 'Number' },
+          },
+        });
       } else res.status(500).json({ error: 'No ID Found' });
     })
     .catch((error) => {
