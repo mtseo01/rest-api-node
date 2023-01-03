55cf4bc037a5567858e1d70be8106b10937c5e66
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-03 09:15:40 +0900

    Add user schema option

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /^([\w\.\_\-])*[a-zA-Z0-9]+([\w\.\_\-])*([a-zA-Z0-9])+([\w\.\_\-])+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,8}$/,
+  },
   password: { type: String, required: true },
 });
 
