435bf905e6000d5ae84121bad6bc6ec0430cae1d
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-03 16:38:25 +0900

    Feat Login & JWT

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next) => {
   User.find({ email: req.body.email })
@@ -36,6 +37,88 @@ router.post('/signup', (req, res, next) => {
         });
       }
     });
+});
+
+router.post('/login', (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(404).json({
+          message: "email not found, user doesn't exist",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({ message: 'Auth failed' });
+        } else if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: '1h',
+            }
+          );
+          return res.status(200).json({ message: 'Auth successful', token });
+        } else {
+          res.status(401).json({ message: 'Auth failed' });
+        }
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.get('/', (req, res, next) => {
+  User.find()
+    .select('_id email password')
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        users: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            email: doc.email,
+            password: doc.password,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/user/' + doc._id,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.get('/:userId', (req, res, next) => {
+  User.findById({ _id: req.params.userId })
+    .exec()
+    .then((user) => {
+      res.status(200).json({
+        user,
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/user',
+        },
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.delete('/:userId', (req, res, next) => {
+  User.deleteOne({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: 'User deleted',
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
